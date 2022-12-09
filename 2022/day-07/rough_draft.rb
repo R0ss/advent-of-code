2b03f42eafbfa954ef2e77008aa7e18aa069026d
Ross Gaith <ross.gatih@gmail.com>
2022-12-09 08:48:47 -0500

    simplified logic

--- 2022/day-07/rough_draft.rb
+++ 2022/day-07/rough_draft.rb
@@ -7,15 +7,15 @@ require 'benchmark'
 include Benchmark 
 
 def rough_draft
-  File.open('2022/day-07/input.txt','r') do |f|
+  File.open('input.txt','r') do |f|
 
       curr_dir = []
       sizes = {}
       sizes.default = 0;
 
       while ln = f.gets&.split(' ')
-        curr_dir = ['~'] if ln == ['$', 'cd', '/']
-        curr_dir.pop if ln == ['$', 'cd', '..']
+        curr_dir = ['~'] if ln[2] == '/'
+        curr_dir.pop if ln[2] == '..'
         curr_dir << ln[2] if (ln - ['/', '..']).length > 2
 
         curr_dir.length.times { |i| sizes[curr_dir[0, i+1].join] += ln[0].to_i } if ln[0].to_i.is_a? Numeric
